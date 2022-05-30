d02374614a59ccffd9cf8d209d92b9a7d21a9beb
aakash-gaur <gauraakash13@gmail.com>
2022-05-29 23:31:34 -0700

    first commit

---
+++ includes/database_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_hostname = "127.0.0.1";
+$db_username = "root";
+$db_password = "";
+$db_name = "pglife";
+
+// $conn = mysqli_connect("127.0.0.1", "root", "", "pglife");
+$conn = mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
+
+if (!$conn) {
+    // Throw error message based on ajax or not
+    echo "Failed to connect to MySQL! Please contact the admin.";
+    return;
+}
+?>
